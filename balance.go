7bd83f554c8c6c6f2b050ac33384a70a3839c17b
gitforbit <gitforbit@gmail.com>
2019-10-29 16:40:12 +0100

    MEDIUM: balance: balance parameter reworked

--- balance.go
+++ balance.go
@@ -22,7 +22,6 @@ package models
 
 import (
 	"encoding/json"
-	"strconv"
 
 	strfmt "github.com/go-openapi/strfmt"
 
@@ -36,11 +35,45 @@ import (
 type Balance struct {
 
 	// algorithm
-	// Enum: [roundrobin static-rr leastconn first source uri url_param random]
-	Algorithm string `json:"algorithm,omitempty"`
+	// Required: true
+	// Enum: [roundrobin static-rr leastconn first source uri url_param hdr random rdp-cookie]
+	Algorithm *string `json:"algorithm"`
 
-	// arguments
-	Arguments []string `json:"arguments"`
+	// hdr name
+	// Pattern: ^[^\s]+$
+	HdrName string `json:"hdr_name,omitempty"`
+
+	// hdr use domain only
+	HdrUseDomainOnly bool `json:"hdr_use_domain_only,omitempty"`
+
+	// random draws
+	RandomDraws int64 `json:"random_draws,omitempty"`
+
+	// rdp cookie name
+	// Pattern: ^[^\s]+$
+	RdpCookieName string `json:"rdp_cookie_name,omitempty"`
+
+	// uri depth
+	// Pattern: ^[^\d+$]
+	URIDepth int64 `json:"uri_depth,omitempty"`
+
+	// uri len
+	// Pattern: ^[^\d+$]
+	URILen int64 `json:"uri_len,omitempty"`
+
+	// uri whole
+	URIWhole bool `json:"uri_whole,omitempty"`
+
+	// url param
+	// Pattern: ^[^\s]+$
+	URLParam string `json:"url_param,omitempty"`
+
+	// url param check post
+	URLParamCheckPost int64 `json:"url_param_check_post,omitempty"`
+
+	// url param max wait
+	// Pattern: ^[^\d+$]
+	URLParamMaxWait int64 `json:"url_param_max_wait,omitempty"`
 }
 
 // Validate validates this balance
@@ -51,7 +84,27 @@ func (m *Balance) Validate(formats strfmt.Registry) error {
 		res = append(res, err)
 	}
 
-	if err := m.validateArguments(formats); err != nil {
+	if err := m.validateHdrName(formats); err != nil {
+		res = append(res, err)
+	}
+
+	if err := m.validateRdpCookieName(formats); err != nil {
+		res = append(res, err)
+	}
+
+	if err := m.validateURIDepth(formats); err != nil {
+		res = append(res, err)
+	}
+
+	if err := m.validateURILen(formats); err != nil {
+		res = append(res, err)
+	}
+
+	if err := m.validateURLParam(formats); err != nil {
+		res = append(res, err)
+	}
+
+	if err := m.validateURLParamMaxWait(formats); err != nil {
 		res = append(res, err)
 	}
 
@@ -65,7 +118,7 @@ var balanceTypeAlgorithmPropEnum []interface{}
 
 func init() {
 	var res []string
-	if err := json.Unmarshal([]byte(`["roundrobin","static-rr","leastconn","first","source","uri","url_param","random"]`), &res); err != nil {
+	if err := json.Unmarshal([]byte(`["roundrobin","static-rr","leastconn","first","source","uri","url_param","hdr","random","rdp-cookie"]`), &res); err != nil {
 		panic(err)
 	}
 	for _, v := range res {
@@ -96,8 +149,14 @@ const (
 	// BalanceAlgorithmURLParam captures enum value "url_param"
 	BalanceAlgorithmURLParam string = "url_param"
 
+	// BalanceAlgorithmHdr captures enum value "hdr"
+	BalanceAlgorithmHdr string = "hdr"
+
 	// BalanceAlgorithmRandom captures enum value "random"
 	BalanceAlgorithmRandom string = "random"
+
+	// BalanceAlgorithmRdpCookie captures enum value "rdp-cookie"
+	BalanceAlgorithmRdpCookie string = "rdp-cookie"
 )
 
 // prop value enum
@@ -110,30 +169,91 @@ func (m *Balance) validateAlgorithmEnum(path, location string, value string) err
 
 func (m *Balance) validateAlgorithm(formats strfmt.Registry) error {
 
-	if swag.IsZero(m.Algorithm) { // not required
-		return nil
+	if err := validate.Required("algorithm", "body", m.Algorithm); err != nil {
+		return err
 	}
 
 	// value enum
-	if err := m.validateAlgorithmEnum("algorithm", "body", m.Algorithm); err != nil {
+	if err := m.validateAlgorithmEnum("algorithm", "body", *m.Algorithm); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func (m *Balance) validateArguments(formats strfmt.Registry) error {
+func (m *Balance) validateHdrName(formats strfmt.Registry) error {
 
-	if swag.IsZero(m.Arguments) { // not required
+	if swag.IsZero(m.HdrName) { // not required
 		return nil
 	}
 
-	for i := 0; i < len(m.Arguments); i++ {
+	if err := validate.Pattern("hdr_name", "body", string(m.HdrName), `^[^\s]+$`); err != nil {
+		return err
+	}
 
-		if err := validate.Pattern("arguments"+"."+strconv.Itoa(i), "body", string(m.Arguments[i]), `^[^\s]+$`); err != nil {
-			return err
-		}
+	return nil
+}
 
+func (m *Balance) validateRdpCookieName(formats strfmt.Registry) error {
+
+	if swag.IsZero(m.RdpCookieName) { // not required
+		return nil
+	}
+
+	if err := validate.Pattern("rdp_cookie_name", "body", string(m.RdpCookieName), `^[^\s]+$`); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (m *Balance) validateURIDepth(formats strfmt.Registry) error {
+
+	if swag.IsZero(m.URIDepth) { // not required
+		return nil
+	}
+
+	if err := validate.Pattern("uri_depth", "body", string(m.URIDepth), `^[^\d+$]`); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (m *Balance) validateURILen(formats strfmt.Registry) error {
+
+	if swag.IsZero(m.URILen) { // not required
+		return nil
+	}
+
+	if err := validate.Pattern("uri_len", "body", string(m.URILen), `^[^\d+$]`); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (m *Balance) validateURLParam(formats strfmt.Registry) error {
+
+	if swag.IsZero(m.URLParam) { // not required
+		return nil
+	}
+
+	if err := validate.Pattern("url_param", "body", string(m.URLParam), `^[^\s]+$`); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (m *Balance) validateURLParamMaxWait(formats strfmt.Registry) error {
+
+	if swag.IsZero(m.URLParamMaxWait) { // not required
+		return nil
+	}
+
+	if err := validate.Pattern("url_param_max_wait", "body", string(m.URLParamMaxWait), `^[^\d+$]`); err != nil {
+		return err
 	}
 
 	return nil
