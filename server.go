f5af0a5ab0ea5b9d34f6b528daef66ec8b8885e1
Jean-Louis Dupond <jean-louis@dupond.be>
2020-01-27 13:54:27 +0100

    MINOR: server: Add SNI stuff

--- server.go
+++ server.go
@@ -71,6 +71,7 @@ type Server struct {
 	Check string `json:"check,omitempty"`
 
 	// check sni
+	// Pattern: ^[^\s]+$
 	CheckSni string `json:"check-sni,omitempty"`
 
 	// check ssl
@@ -137,6 +138,7 @@ type Server struct {
 	SendProxyV2 string `json:"send-proxy-v2,omitempty"`
 
 	// sni
+	// Pattern: ^[^\s]+$
 	Sni string `json:"sni,omitempty"`
 
 	// ssl
